2cb181d87bce4ccce26c1a6580d55a267dffeb7f
Bas van Driel <contact@basvandriel.nl>
2021-03-26 22:43:05 +0100

    begin of layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,30 @@ import './App.css';
 
 function App() {
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path={['/', 'dashboard']}>
-            <Header />
-          {/* Cards */}
-          {/* Buttons */}
+    <div className='relative bg-grey'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6'>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={['/', 'dashboard']}>
+              <Header />
+            {/* Cards */}
+            {/* Buttons */}
+            </Route>
+          <Route exact path='/cards' component={Cards} />
+          <Route exact path='/react'>
+            <div className='App'>
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Swipet - A home for your pet
+                  </p>
+                </header>
+              </div>
           </Route>
-        <Route exact path='/cards' component={Cards} />
-        <Route exact path='/react'>
-          <div className='App'>
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Swipet - A home for your pet
-                </p>
-              </header>
-            </div>
-        </Route>
-        </Switch>
-      </BrowserRouter>
+          </Switch>
+          </BrowserRouter>    
+       </div>
+      </div>
   );
 }
 
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,9 +1,14 @@
-import { FC, Fragment} from "react";
+import { FC } from "react";
+import { HiUser } from 'react-icons/hi'
 
 const Header: FC = () => (
-  <Fragment>
-      <h2>I'm a header</h2>
-  </Fragment>
-);
+  <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+    <nav className='hidden md:flex space-x-10'>
+      <button type='button' className='text-gray-500 group bg-white inline-flex items-center text-base font-medium hover:text-gray-900 focus-outline:none focus:outline-none' aria-expanded='false'>
+        <HiUser/>
+      </button>
+    </nav>
+  </div>
+)
 
 export default Header;
