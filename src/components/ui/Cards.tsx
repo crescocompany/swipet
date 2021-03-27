ab3c6e5aa932d0d91fe964cfc504f1bf4992e5e6
Bas van Driel <contact@basvandriel.nl>
2021-03-27 19:05:11 +0100

    Add sample functions

--- src/components/ui/Cards.tsx
+++ src/components/ui/Cards.tsx
@@ -15,6 +15,16 @@ const Cards: FC = () => {
       .then(data => setPets(data['message']));
   }, [setPets])
 
+  const decline = () => alert('Not for me!');
+
+  const like = (superpower = false) => {
+    alert('Liked!');
+
+    if (superpower) {
+      alert('by power!');
+    }
+  };
+
   return (
     <div className="p-10">
       {pets.map((pet, index) => <Card key={index} img={pet} />)}
@@ -26,15 +36,15 @@ const Cards: FC = () => {
           </button>
         )}
 
-        <button className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-bold p-4 mr-4 rounded-xl inline-flex align-middle">
+        <button className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-bold p-4 mr-4 rounded-xl inline-flex align-middle" onClick={() => decline()}>
           <HiX className='fill-current w-8 h-8 text-red' />
         </button>
 
-        <button className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-bold p-4 mr-4 rounded-xl inline-flex align-middle">
+        <button className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-bold p-4 mr-4 rounded-xl inline-flex align-middle" onClick={() => like(true)}>
           <HiStar className='fill-current w-5 h-5 text-blue' />
         </button>
 
-        <button className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-bold p-4 mr-4 rounded-xl inline-flex align-middle">
+        <button className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-bold p-4 mr-4 rounded-xl inline-flex align-middle" onClick={() => like()}>
           <HiHeart className='fill-current w-8 h-8 text-green' />
         </button>
       </div>
